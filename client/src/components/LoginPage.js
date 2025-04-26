6b18a2a7734b99c9b810070408d540e19e74c53c
Andrew Ayson <drewubaldeayson@gmail.com>
2025-04-26 15:40:53 +0800

    updates

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -23,7 +23,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', { 
+      const response = await axios.post('https://jumarisllc.com/api/auth/login', { 
         email, 
         password 
       });
